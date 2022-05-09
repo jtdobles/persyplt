bf6b6274ccd0a98a4cc32f2544f8fed3f654cb9a
Javier Dobles <33819692+jtdobles@users.noreply.github.com>
2022-05-09 17:16:46 -0400

    Delete _build directory

--- _build/test.ml
+++
@@ -1,8 +0,0 @@
-(* To do: edit this code to make it work with our parser, scanner & semant *)
-open Ast
-
-let _ =
-  let lexbuf = Lexing.from_channel stdin in
-  let program = Parser.program Scanner.tokenize lexbuf in
-  let sprogram = Semant.check program in
-  print_endline (string_of_program sprogram)
